a1d820f5cbfe1a131a9596bf5ba571edd2b01d16
Trippy <138500712+Trippy07@users.noreply.github.com>
2023-10-19 02:30:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -177,7 +177,7 @@ async def get_users(client: Bot, message: Message):
     await msg.edit(f"{len(users)} <b>Usᴇʀs ᴜsᴇ ᴛʜɪs ʙᴏᴛ</b>")
 
 
-@Bot.on_message(filters.command("broadcast") & filters.user(ADMINS, 5493260414))
+@Bot.on_message(filters.command("broadcast") & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
         query = await query_msg()
